a057231821217b48cb1dc6650eb8b62d3e59ada5
aditya <adityabhawsingka@gmail.com>
2019-10-30 17:32:05 +0530

    schedule dashboard load after 2 seconds to prevent blank screen.

--- app/startupscreen.py
+++ app/startupscreen.py
@@ -8,7 +8,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.app import App
 from kivy.lang import Builder
 
-startupscreen = '''
+startupscreen = """
 <StartupScreen>
     name: 'StartupScreen'
     canvas.before:
@@ -26,7 +26,7 @@ startupscreen = '''
         size_hint_x: .8
         pos_hint : {'center_x': .5, 'center_y': .5}
         
-'''
+"""
 
 
 class StartupScreen(Screen):
@@ -39,7 +39,8 @@ class StartupScreen(Screen):
 
     def on_enter(self, *args):
         from kivy.clock import Clock
-        Clock.schedule_once(lambda dt: self.load_navigation(), 0)
+
+        Clock.schedule_once(lambda dt: self.load_navigation(), 2)
 
     @staticmethod
     def load_navigation():
@@ -47,17 +48,19 @@ class StartupScreen(Screen):
 
         """Set the app theme settings"""
         from kivymd.theming import ThemeManager
+
         app = App.get_running_app()
         app.theme_cls = ThemeManager()
-        app.theme_cls.primary_palette = 'Teal'
-        app.theme_cls.accent_palette = 'Amber'
-        app.theme_cls.theme_style = 'Light'
-        app.theme_cls.theme_style = app.config.get('CustSettings', 'mode')
+        app.theme_cls.primary_palette = "Teal"
+        app.theme_cls.accent_palette = "Amber"
+        app.theme_cls.theme_style = "Light"
+        app.theme_cls.theme_style = app.config.get("CustSettings", "mode")
 
         from screens import Screens
+
         app.screens = Screens()
         app.screens.app = app
 
         app.screens.primary_widget = NavigationScreen()
         app.root.add_widget(app.screens.primary_widget)
-        app.root.current = 'NavigationScreen'
+        app.root.current = "NavigationScreen"
